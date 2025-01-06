bd46b791a2008df7638bf474720dbb0f2fe69392
Parth Shah <137877398+parthshah-dev@users.noreply.github.com>
2025-01-06 18:30:36 +0530

    Add files via upload

---
+++ MIC/Hello.asm
@@ -0,0 +1,17 @@
+section .data
+	message db 'Hello, World!', 10
+	msg_len equ $ - message
+	
+section .text
+global _start
+_start:
+	mov eax, 4
+	mov ebx, 1
+	mov ecx, message
+	mov edx, msg_len
+	int 80h
+	
+	mov eax, 1
+	xor ebx, ebx
+	int 80h
+	
